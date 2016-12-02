072c3443f6669c9afa1c60d5bc081d7e9da7d4c7
Igor Ming <igormmesquita@ymail.com>
2016-12-02 02:19:51 -0200

    improvment

--- public/js/script.js
+++ public/js/script.js
@@ -1,16 +1,42 @@
+const BASE_URL = 'https://magnetis-trades.herokuapp.com';
+
 $(document).ready(function() {
 
   $('#add-new').click(function(e) {
     e.preventDefault();
-    addNew();
+    addNew(null, true);
   });
+
+  $('#form-investments').submit(function(e) {
+    e.preventDefault();
+
+    $('#form-investments tr').each(function(i, v) {
+      console.log(i, v);
+    })
+  })
 
   fetchInvestments();
 });
 
-function fetchInvestments() {
-  const BASE_URL = 'https://magnetis-trades.herokuapp.com';
+function getPrice(date, fund_id, success) {
+  // if (typeof(date) === 'object'){
+  //   console.log(date);
+  // }
 
+  let splittedDate = date.split('/');
+  if (splittedDate.length === 3) {
+    date = `${splittedDate[2]}-${splittedDate[1]}-${splittedDate[0]}`;
+  }
+
+  return $.ajax({
+    method: 'GET',
+    dataType: 'json',
+    url: `${BASE_URL}/funds/${fund_id}/prices.json?date=${date}`,
+    success
+  })
+}
+
+function fetchInvestments() {
   $.ajax({
     method: 'GET',
     dataType: 'json',
@@ -19,47 +45,60 @@ function fetchInvestments() {
       $.each(tradesResponse, function(i, trade) {
         const { date, fund_id, kind, shares } = trade;
 
-        $.ajax({
-          method: 'GET',
-          dataType: 'json',
-          url: `${BASE_URL}/funds/${fund_id}/prices.json?date=${date}`,
-          success: function(pricesResponse) {
-            const investmentObj = {
-              date,
-              kind,
-              shares,
-              price: pricesResponse.price,
-            };
+        getPrice(date, fund_id, function(pricesResponse) {
+          const investmentObj = {
+            date,
+            kind,
+            shares,
+            price: pricesResponse.price,
+          };
 
-            addNew(investmentObj);
-          }
-        })
-      })
+          addNew(investmentObj, false);
+        });
+      });
     }
   });
 }
 
-function addNew(elem) {
+function addNew(elem, isNew) {
   const rowLength = $('#investments tbody tr').length;
   let indexToAdd = 0;
 
   if (rowLength === 0) {
     indexToAdd = 1
-    $('#investments tbody').append(getRowScope(indexToAdd, elem));
+    $('#investments tbody').append(getRowScope(indexToAdd, elem, isNew));
   } else {
     indexToAdd = Number($('#investments tbody tr:last').attr('id'))+1;
-    $('#investments tbody tr:last').after(getRowScope(indexToAdd, elem));
+    $('#investments tbody tr:last').after(getRowScope(indexToAdd, elem, isNew));
   }
 
   /*
     Set after html rendering
   */
-  let date = new Date(elem.date);
+  let date = new Date();
+
+  if (!isNew) {
+    date = new Date(elem.date);
+  }
+
   date.setDate(date.getDate()+1);
 
   const datePicker = $(`#${indexToAdd} .date`);
   datePicker.datepicker($.datepicker.regional['pt-BR']);
   datePicker.datepicker('setDate', date);
+
+  $(datePicker).change(function(e){
+    getPrice(e.target.value, 1, function(response) {
+      if (!response) {
+        alert('Não há cotas para esta data.');
+        return;
+      }
+
+      console.log(response.price);
+      $(`#${indexToAdd} .price`).val(response.price )
+    })
+  })
+
 }
 
 function selectedRow(elem) {
@@ -73,25 +112,30 @@ function deleteAction(elem) {
 }
 
 function calculate(shares, price) {
-  return shares * price || 0;
+  return getCurrency(shares * price) || 0;
 }
 
-function calcKeyup(elem, price) {
+function calcKeyup(elem) {
   const rowId = $(elem).closest('tr').attr('id');
+  const price = $(`#price-${rowId}`).val();
 
   $(`#total-${rowId}`).val(calculate(elem.value, price));
 }
 
-function getRowScope(index, elem) {
+function getCurrency(value) {
+  return Number(value).toLocaleString('pt-BR') || 0;
+}
+
+function getRowScope(index, elem, isNew) {
   const {
     date,
     kind,
     price,
     shares,
   } = elem || {};
-console.log(elem);
+
   return `
-  <tr id=${index} onClick="selectedRow(this);">
+  <tr id=${index} isNew=${isNew} onClick="selectedRow(this);">
     <td>
       <img src="images/${getImage(kind)}" alt="">
     </td>
@@ -106,10 +150,10 @@ console.log(elem);
       </select>
     </td>
     <td>
-      <input class="form-control" onkeyup="calcKeyup(this, ${price || 0});" type="text" value=${shares || 0}>
+      <input class="form-control" onkeyup="calcKeyup(this, ${price});" type="number" value=${shares || 0}>
     </td>
     <td>
-      <input class="form-control" type="text" value=${price || 0} disabled>
+      <input id="price-${index}" class="form-control price" type="text" value=${price} disabled>
     </td>
     <td>
       <input id="total-${index}" class="form-control" type="text" value=${calculate(shares, price)} disabled>
@@ -123,7 +167,8 @@ console.log(elem);
 
 function getImage(kind) {
   switch(kind) {
-    case 0: {
+    case 0:
+    default: {
       return 'aplicacao.png';
     }
     case 1: {
